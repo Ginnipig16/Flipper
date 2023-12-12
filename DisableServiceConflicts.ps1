2dd5a84b238c443b7b2bf4d0262e9358d1a1a315
Ginnipig16 <112461867+Ginnipig16@users.noreply.github.com>
2023-12-13 09:49:44 +1100

    Add files via upload

---
+++ DisableServiceConflicts.ps1
@@ -0,0 +1,7 @@
+﻿# Disable the services
+Set-Service -Name 'hns' -StartupType 'Disabled'
+Set-Service -Name 'SharedAccess' -StartupType 'Disabled'
+
+# Stop the services
+Stop-Service -Name 'hns' -Force
+Stop-Service -Name 'SharedAccess' -Force
