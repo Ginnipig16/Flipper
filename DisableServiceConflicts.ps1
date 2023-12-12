bf92aae546843312ae8bb63e94182771d16c3c47
Ginnipig16 <112461867+Ginnipig16@users.noreply.github.com>
2023-12-13 09:57:15 +1100

    Update DisableServiceConflicts.ps1

--- DisableServiceConflicts.ps1
+++ DisableServiceConflicts.ps1
@@ -1,7 +1,10 @@
 ﻿# Disable the services
 Set-Service -Name 'hns' -StartupType 'Disabled'
 Set-Service -Name 'SharedAccess' -StartupType 'Disabled'
+Write-Host "Set Host Network Service to DISABLED" -ForegroundColor Green
+Write-Host "Set Internet connection sharing to DISABLED" -ForegroundColor Green
 
 # Stop the services
 Stop-Service -Name 'hns' -Force
 Stop-Service -Name 'SharedAccess' -Force
+Write-Host "Stopping the services complete" -ForegroundColor Green
